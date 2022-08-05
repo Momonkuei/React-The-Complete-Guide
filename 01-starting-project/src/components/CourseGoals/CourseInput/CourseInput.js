7950be6a2e85cf95df5a0926d79cc2b54b723c40
Jackson Chen <momonbin1991@icloud.com>
2022-08-06 07:28:22 +0800

    2022080475. Setting Dynamic Inline Styles設置動態內聯樣式完成

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -4,26 +4,49 @@ import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+	const [enteredValue, setEnteredValue] = useState('');
+	const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+	const goalInputChangeHandler = event => {
+		/* 當判斷的值長度 > 0 時 把 isValid 轉為true值 */
+		if (event.target.value.trim().length() > 0) {
+			setIsValid(true);
+		}
+		setEnteredValue(event.target.value);
+	};
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+	const formSubmitHandler = event => {
+		event.preventDefault();
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+		if (enteredValue.trim().length === 0) {
+			setIsValid(false);
+			return;
+		}
+
+		props.onAddGoal(enteredValue);
+		setEnteredValue('');
+	};
+
+	return (
+		<form onSubmit={formSubmitHandler}>
+			<div className="form-control">
+				<label style={{ color: !isValid ? 'red' : 'black' }}>
+					Course Goal
+				</label>
+				<input
+					style={{
+						borderColor: !isValid ? 'red' : '#ccc',
+						background: !isValid ? 'salmon' : 'transparent',
+					}}
+					// 現在狀態是 行內樣式(Inline styles) 層級是最高的
+					type="text"
+					value={enteredValue}
+					onChange={goalInputChangeHandler}
+				/>
+			</div>
+			<Button type="submit">Add Goal</Button>
+		</form>
+	);
 };
 
 export default CourseInput;
