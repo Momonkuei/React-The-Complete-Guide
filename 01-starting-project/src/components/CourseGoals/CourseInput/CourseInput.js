1c0742679a4078ad6e7cd21d3ca41c1d19a98b6c
Jackson Chen <momonbin1991@icloud.com>
2022-08-07 18:56:37 +0800

    79. Styled Components & Media Queries樣式化組件和媒體查詢完成

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,35 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+const FormControl = styled.div`
+	margin: 0.5rem 0;
+
+	& label {
+		font-weight: bold;
+		display: block;
+		margin-bottom: 0.5rem;
+		color: ${props => (props.inValid ? 'red' : 'black')};
+	}
+
+	& input {
+		display: block;
+		width: 100%;
+		border: 1px solid ${props => (props.isValid ? 'red' : '#ccc')};
+		background:${props => (props.inValid ? '#ffd7d7' : 'transparent')}
+		font: inherit;
+		line-height: 1.5rem;
+		padding: 0 0.25rem;
+	}
+
+	& input:focus {
+		outline: none;
+		background: #fad0ec;
+		border-color: #8b005d;
+	}
+`;
 
 const CourseInput = props => {
 	const [enteredValue, setEnteredValue] = useState('');
@@ -27,26 +55,56 @@ const CourseInput = props => {
 		setEnteredValue('');
 	};
 
+	/************* */
+	/* 修改後的做法-方法二 */
+	/************* */
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<div className="form-control">
-				<label style={{ color: !isValid ? 'red' : 'black' }}>
-					Course Goal
-				</label>
+			<FormControl inValid={!isValid}>
+				<label>Course Goal</label>
 				<input
-					style={{
-						borderColor: !isValid ? 'red' : '#ccc',
-						background: !isValid ? 'salmon' : 'transparent',
-					}}
-					// 現在狀態是 行內樣式(Inline styles) 層級是最高的
 					type="text"
 					value={enteredValue}
 					onChange={goalInputChangeHandler}
 				/>
-			</div>
+			</FormControl>
 			<Button type="submit">Add Goal</Button>
 		</form>
 	);
+
+	// /************* */
+	// /* 修改後的做法-方法一 */
+	// /************* */
+	// return (
+	// 	<form onSubmit={formSubmitHandler}>
+	// 		<FormControl className={!isValid && 'invalid'}>
+	// 			<label>Course Goal</label>
+	// 			<input
+	// 				type="text"
+	// 				value={enteredValue}
+	// 				onChange={goalInputChangeHandler}
+	// 			/>
+	// 		</FormControl>
+	// 		<Button type="submit">Add Goal</Button>
+	// 	</form>
+	// );
+
+	/************* */
+	/* 原本去判斷輸入是否為空值蓋變樣式的做法 */
+	/************* */
+	// return (
+	// 	<form onSubmit={formSubmitHandler}>
+	// 		<div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+	// 			<label>Course Goal</label>
+	// 			<input
+	// 				type="text"
+	// 				value={enteredValue}
+	// 				onChange={goalInputChangeHandler}
+	// 			/>
+	// 		</div>
+	// 		<Button type="submit">Add Goal</Button>
+	// 	</form>
+	// );
 };
 
 export default CourseInput;
