365641114d4000c8afc114e65ffd734a45af50ac
Jackson Chen <momonbin1991@icloud.com>
2022-08-10 07:46:46 +0800

    20220810  93. Managing the User Input State篇章完成

--- Time to Practice/practice-project-create-form/src/components/Users/AddUser.js
+++ Time to Practice/practice-project-create-form/src/components/Users/AddUser.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+
+import classes from './AddUser.module.css';
 
 const AddUser = props => {
-	const addUserHandler = () => {};
+	const [enterUsername, setEnteredUsername] = useState('');
+	const [enterAge, setEnteredAge] = useState('');
+
+	const usernameChangeHandler = event => {
+		setEnteredUsername(event.target.value);
+	};
+
+	const ageChangeHandler = event => {
+		setEnteredAge(event.target.value);
+	};
+
+	const addUserHandler = event => {
+		event.preventDefault();
+		console.log(enterUsername, enterAge);
+		setEnteredAge('');
+		setEnteredUsername('');
+	};
 
 	return (
-		<form onSubmit={addUserHandler}>
-			<label htmlFor="username">Username</label>
-			<input id="username" type="text" />
-			<label htmlFor="age">Age(Years)</label>
-			<input id="age" type="number" />
-			<button type="submit">新增資料</button>
-		</form>
+		<Card className={classes.input}>
+			<form onSubmit={addUserHandler}>
+				<label htmlFor="username">Username</label>
+				<input
+					id="username"
+					type="text"
+					value={enterUsername}
+					onChange={usernameChangeHandler}
+				/>
+				<label htmlFor="age">Age(Years)</label>
+				<input
+					id="age"
+					type="number"
+					value={enterAge}
+					onChange={ageChangeHandler}
+				/>
+				<Button type="submit">新增資料</Button>
+			</form>
+		</Card>
 	);
 };
 
